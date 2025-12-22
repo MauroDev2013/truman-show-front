6414aeaa8dee143382af935ae0521339ebc5f28c
MauroDev2013 <mauro2013oliv@gmail.com>
2025-12-22 14:14:24 -0400

    [UPDATE] new story

--- src/data/teoriasStories.js
+++ src/data/teoriasStories.js
@@ -13,5 +13,12 @@ export const teoriasStories = [
     type: "conspiracy",
     position: [60.0035, 24.6478],
     content: "My grandfather was at the port of Tallinn that morning. People were boarding the ship. He saw soldiers loading something onto one part of the ship; it looked like they were arranging cargo, but it was a civilian ship. He had a bad feeling and didn't board. Hours later, the ship sank, killing 852 people. Besides him, other people also witnessed it. The ship was sabotaged. Some who tried to tell the story disappeared. A helicopter would appear, the person would be taken away, and never seen again. My grandfather was scared and didn't want to tell what he saw for a long time.",
+  },
+  {
+  id: 3,
+    title: "The Varginha ET",
+    type: "alien",
+    position: [-21.5464, -45.4308],
+    content: "In January 1996, the city of Varginha seemed strangely silent. Residents commented that, hours earlier, something luminous had crossed the sky and disappeared behind the hills. Shortly after, three girls were returning home when they saw something they had never seen before: crouched near a plot of land, was a small being, with dark, shiny skin, large, reddish eyes. It seemed frightened, motionless, as if it were lost. The girls ran in panic and told what they had seen. From then on, strange things began to happen. During the night, Army trucks passed through the streets, something unusual for a small city. Witnesses say that soldiers surrounded specific areas, mainly near hospitals and remote plots of land. It was said that similar creatures had been seen in other parts of the city. In the early morning, men who did not seem like ordinary soldiers began to appear. They dressed discreetly, almost always in black, spoke little, and did not identify themselves. According to reports, they arrived along with the military and took control of the situation. Some residents claim to have seen beings being placed in closed vehicles, covered by tarpaulins, and quickly taken out of the city. Hospitals were reportedly isolated, records altered, and witnesses instructed to remain silent. The following day, authorities said nothing abnormal had occurred. No ETs, no strange objects. Just 'confusion' and 'misunderstandings.' But for those who lived through that night, the feeling was different:that something much bigger than the city had passed through and was hastily taken away. Since then, Varginha has never been the same.",
   }
 ];
